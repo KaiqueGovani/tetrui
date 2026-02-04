393cd0b0439aa9f9b5b1f6d58e975d7b49cd3fd2
KaiqueGovani <kaique.govani@hotmail.com>
2026-02-03 23:43:13 -0300

    Add release workflow and curl installer

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+set -e
+
+REPO="${TETRUI_REPO:-tetrui/tetrui}"
+VERSION="${TETRUI_VERSION:-nightly}"
+DEST="${DEST:-$HOME/.local/bin/tetrui}"
+
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+
+case "$OS" in
+  linux) OS="linux" ;;
+  darwin) OS="darwin" ;;
+  *)
+    echo "Unsupported OS: $OS"
+    exit 1
+    ;;
+esac
+
+case "$ARCH" in
+  x86_64|amd64) ARCH="amd64" ;;
+  arm64|aarch64) ARCH="arm64" ;;
+  *)
+    echo "Unsupported arch: $ARCH"
+    exit 1
+    ;;
+esac
+
+BIN="tetrui-${OS}-${ARCH}"
+URL="https://github.com/${REPO}/releases/download/${VERSION}/${BIN}"
+
+mkdir -p "$(dirname "$DEST")"
+curl -fsSL "$URL" -o "$DEST"
+chmod +x "$DEST"
+
+echo "Installed tetrui to $DEST"
