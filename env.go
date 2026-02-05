316172fdddd4eb44d0d5cb705abf21d926327689
KaiqueGovani <kaique.govani@hotmail.com>
2026-02-05 02:37:36 -0300

    Embed score sync defaults via build flags

--- env.go
+++ env.go
@@ -1,3 +1,21 @@
 package main
 
-func loadEmbeddedEnv() {}
+import "os"
+
+var (
+	defaultScoreAPIURL string
+	defaultScoreAPIKey string
+)
+
+func loadEmbeddedEnv() {
+	if defaultScoreAPIURL != "" {
+		if _, exists := os.LookupEnv("TETRUI_SCORE_API_URL"); !exists {
+			_ = os.Setenv("TETRUI_SCORE_API_URL", defaultScoreAPIURL)
+		}
+	}
+	if defaultScoreAPIKey != "" {
+		if _, exists := os.LookupEnv("TETRUI_SCORE_API_KEY"); !exists {
+			_ = os.Setenv("TETRUI_SCORE_API_KEY", defaultScoreAPIKey)
+		}
+	}
+}
